b73efb727ad8260fddfb45a2d08c00f4a0b0f143
vishal844579 <94944815+vishal844579@users.noreply.github.com>
2021-11-26 03:36:19 +0000

    n

--- p1original.c
+++ p1original.c
@@ -2,8 +2,9 @@
 int main()
 {
   int a,b,c;
-  printf("add two numbers\n");
-  scanf("%d%d%d");
-  a+b=c;
+  printf("enter the numbers\n");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  printf("%d+%d=%d\n",a,b,c);
   return 0;
 }
