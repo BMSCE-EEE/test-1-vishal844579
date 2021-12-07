1895d355f4488c800dac621838dab194ff291883
vishal844579 <94944815+vishal844579@users.noreply.github.com>
2021-12-07 02:56:46 +0000

    nil

---
+++ p8final.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  int a;
+  float ans;
+  printf("enter the number\n");
+  scanf("%d",&a);
+  ans=sqrt(a);
+  printf("the squareroot of number %d is %f",a,ans);
+  return 0;
+}
