df5164326d306bb61fa8fdb9d206e915d4ac02c4
vishal844579 <94944815+vishal844579@users.noreply.github.com>
2021-12-09 03:06:29 +0000

    nil

--- p9original.c
+++ p9original.c
@@ -1,1 +1,15 @@
- 
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char a[100],b[200];
+  printf("enter the first string\n");
+  fgets(a,100,stdin);
+  printf("enter the second string\n");
+  fgets(b,200,stdin);
+  if (strcmp(a,b)==0)
+  printf("strings are equal\n");
+  else
+  printf("strings are not equal\n");
+  return 0;
+}
